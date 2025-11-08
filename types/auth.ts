3af40a3dfd15d6fabae61231748909025004c06d
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-11-08 22:36:18 +0530

    feat: add types

--- types/auth.ts
+++ types/auth.ts
@@ -3,3 +3,32 @@ export interface LoginFormData {
   password: string;
   expiresInMins?: number;
 }
+
+export interface LoginResponse {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface AuthUser {
+  id: number;
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface AuthState {
+  user: AuthUser | null;
+  tokens: AuthTokens;
+  expiresInMins?: number;
+}
