70646a9ce52f7dce7f42b2a2d11e8047c9f6a17e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-11-21 13:55:18 +0530

    feat: add wmat apis

---
+++ api/wmataApiSlice.ts
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const wmataApiKey = process.env.EXPO_PUBLIC_WMATA_API_PRIMARY_KEY;
+
+export const wmataApiSlice = createApi({
+  reducerPath: 'wmataApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.wmata.com',
+    prepareHeaders: (headers) => {
+      if (wmataApiKey) {
+        headers.set('api_key', wmataApiKey);
+      }
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getStationInfo: builder.query({
+      query: (stationCode) =>
+        `/Rail.svc/json/jStationInfo?StationCode=${stationCode}`,
+    }),
+    getStations: builder.query({
+      query: () => '/Rail.svc/json/jStations',
+    }),
+    getStationParking: builder.query({
+      query: (stationCode) =>
+        `/Rail.svc/json/jStationParking?StationCode=${stationCode}`,
+    }),
+    getStationTimes: builder.query({
+      query: (stationCode) =>
+        `/Rail.svc/json/jStationTimes?StationCode=${stationCode}`,
+    }),
+  }),
+});
+
+export const {
+  useGetStationInfoQuery,
+  useGetStationsQuery,
+  useGetStationParkingQuery,
+  useGetStationTimesQuery,
+} = wmataApiSlice;
